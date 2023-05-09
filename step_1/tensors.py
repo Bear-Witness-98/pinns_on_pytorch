74a3bb996f0c77423b2af30d3d4a4b07f4ad1fae
Santiago Suárez <ssuarez@bernie.tryolabs.com>
2023-05-09 14:48:07 +0000

    Add reqs and first code

--- step_1/tensors.py
+++ step_1/tensors.py
@@ -1,8 +1,19 @@
+import torch
+import numpy
 
+def tensor_init():
+    data = [[1,2],[3,4]]
+    print(data)
 
+    x_data = torch.tensor(data)
+    print(x_data)
+    
 
 def main():
-    print('I am here')
+    print('Here the processing starts')
+
+    tensor_init()
+
 
 
 
