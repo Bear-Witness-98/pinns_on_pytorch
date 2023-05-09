d9bd96198bb15528710b9143cd6565aafc2bb21d
Santiago <santiago@tryolabs.com>
2023-05-09 19:43:51 +0000

    Fix formatting on step2

--- step_2/datasets_dataloaders.py
+++ step_2/datasets_dataloaders.py
@@ -14,26 +14,25 @@ from torch.utils.data import DataLoader
 
 
 def loading_a_dataset():
-
     # this brings the data somehow to the program
 
     training_data = datasets.FashionMNIST(
-        root="data", # path where the data is stored
-        train=True, # specifies training or testing dataset
-        download=True, # download data from the internet if it's not available at root
-        transform=ToTensor() # (or target_transofrmation() ?) specify the feature and label transformations
+        root="data",  # path where the data is stored
+        train=True,  # specifies training or testing dataset
+        download=True,  # download data from the internet if it's not available at root
+        transform=ToTensor(),  # (or target_transofrmation() ?) specify the feature and label transformations
     )
     test_data = datasets.FashionMNIST(
-        root="data", # path where the data is stored
-        train=False, # specifies training or testing dataset
-        download=True, # download data from the internet if it's not available at root
-        transform=ToTensor() # (or target_transofrmation() ?) specify the feature and label transformations
+        root="data",  # path where the data is stored
+        train=False,  # specifies training or testing dataset
+        download=True,  # download data from the internet if it's not available at root
+        transform=ToTensor(),  # (or target_transofrmation() ?) specify the feature and label transformations
     )
 
     # the transform works like so: The data is stored in whatever format (dataset dependand).
     # may be PIL image, jpg image, .mp3 sound, etc. The transform converts it to whatever format
     # we need. It is "sort of" a function that takes each data point in a specific format, and
-    # converts it to a usable data format (e.g. ndarray, tensor, etc.). Usually this function is 
+    # converts it to a usable data format (e.g. ndarray, tensor, etc.). Usually this function is
     # dataset dependant, as different datasets have different formats. But the ToTensor function
     # is pretty versatile in that sense, so may be used for a wide range of cases.
 
@@ -43,47 +42,54 @@ def loading_a_dataset():
 
     return training_data, test_data
 
+
 def iterating_dataset(training_data):
     labels_map = {
-        0:"T-shirt",
-        1:"Trouser",
-        2:"Pullover",
-        3:"Dress",
-        4:"Coat",
-        5:"Sandal",
-        6:"Shirt",
-        7:"Sneakers",
-        8:"Bag",
-        9:"Ankle Boot",
+        0: "T-shirt",
+        1: "Trouser",
+        2: "Pullover",
+        3: "Dress",
+        4: "Coat",
+        5: "Sandal",
+        6: "Shirt",
+        7: "Sneakers",
+        8: "Bag",
+        9: "Ankle Boot",
     }
-    figure = plt.figure(figsize=(8,8))
+    figure = plt.figure(figsize=(8, 8))
     cols, rows = 3, 3
-    for i in range(1, cols*rows+1):
+    for i in range(1, cols * rows + 1):
         sample_idx = torch.randint(len(training_data), size=(1,)).item()
         img, label = training_data[sample_idx]
         figure.add_subplot(rows, cols, i)
         plt.title(labels_map[label])
         plt.axis("off")
-        plt.imshow(img.squeeze(), cmap='gray')
+        plt.imshow(img.squeeze(), cmap="gray")
     # cannot visualize images remotely jeje. Lost some of the magic but guareber.
     plt.show()
+
 
 # a custom Dataset class must impolement three functions: __init__, __len__ and __getitem__.
 # images stored in img_dir (/raw_images)
 # labels in a csv file annotations_file (labels.csv)
 
+
 class CustomImageDataset(Dataset):
     # inits the lables file, the images directory and the transformations to be done (whatever that is) (same as before ?).
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file) # pandas file retaining name and label
-        self.img_dir = img_dir # image directory
-        self.transform = transform # transformation
-        self.target_transform = target_transform # other transformation?
+    def __init__(
+        self, annotations_file, img_dir, transform=None, target_transform=None
+    ):
+        self.img_labels = pd.read_csv(
+            annotations_file
+        )  # pandas file retaining name and label
+        self.img_dir = img_dir  # image directory
+        self.transform = transform  # transformation
+        self.target_transform = target_transform  # other transformation?
 
     # gets the number of items in the dataset.
     def __len__(self):
         return len(self.img_labels)
-    
+
     # loads and returns sa sample from the dataset at index idx.
     ## based on idx, it identifies the image location in disc
     ## converts to tensor using read_image
@@ -91,15 +97,17 @@ class CustomImageDataset(Dataset):
     ## calls the transform function (if applicable)
     ## return tensor image and corresponding label in tuple
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0]) # the join just concatenates strings
-        image = read_image(img_path) # reads an image and converts it to a tensor
-        label = self.img_labels.iloc[idx,1] # gets the label for the given index
+        img_path = os.path.join(
+            self.img_dir, self.img_labels.iloc[idx, 0]
+        )  # the join just concatenates strings
+        image = read_image(img_path)  # reads an image and converts it to a tensor
+        label = self.img_labels.iloc[idx, 1]  # gets the label for the given index
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
-    
+
     # depending on the datatype you have, how this Dataset object should behave.
     # transform and target transform should be similar to the dataset retrieval one.
     # functions defined specifically for the dataset you are working with.
@@ -113,14 +121,17 @@ def custom_dataset():
     sth = new_dataset[2]
     return
 
+
 # after having the dataset class already constructed, we may have a DataLoader class, that
 # is supposed to get us multiple datapoints at a time, reshuffle the data at every epoch, and
 # use python Python's multiprocessing to speed up retrieval.
+
 
 def preparate_dataloaders(training_data, test_data):
     train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
     test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
     return train_dataloader, test_dataloader
+
 
 def main():
     print("Here the processing starts")
@@ -130,19 +141,28 @@ def main():
     iterating_dataset(training_data)
     custom_dataset()
     train_dataloader, test_dataloader = preparate_dataloaders(training_data, test_data)
-    train_features, train_labels = next(iter(train_dataloader)) # why the iter() ?
-    print(f"features type is {type(train_features)}") # a tensor containing all the batch images (64, 1, 28, 28) == (B, C, H, W)
-    print(f'Each feature is of type {type(train_features[0])}') # a tensor containing only one image (1, 28, 28)
-    print(f"labels type is {type(train_labels)}") # idem (label-array tensor)
-    print(f"each label is of type {type(train_labels[0])}") # idem (a single-element tensor)
+    train_features, train_labels = next(iter(train_dataloader))  # why the iter() ?
+    print(
+        f"features type is {type(train_features)}"
+    )  # a tensor containing all the batch images (64, 1, 28, 28) == (B, C, H, W)
+    print(
+        f"Each feature is of type {type(train_features[0])}"
+    )  # a tensor containing only one image (1, 28, 28)
+    print(f"labels type is {type(train_labels)}")  # idem (label-array tensor)
+    print(
+        f"each label is of type {type(train_labels[0])}"
+    )  # idem (a single-element tensor)
     print(f"Feature batch shape {train_features.size()}")
     print(f"Labels batch shape {train_labels.size()}")
 
-    img = train_features[0].squeeze() # what the fuck is a squeeze ? is it for the tensor shape (redundant dimensions)?
+    img = train_features[
+        0
+    ].squeeze()  # what the fuck is a squeeze ? is it for the tensor shape (redundant dimensions)?
     label = train_labels[0]
-    plt.imshow(img, cmap='gray')
+    plt.imshow(img, cmap="gray")
     plt.show()
-    print(f'Label: {label}')
+    print(f"Label: {label}")
+
 
 if __name__ == "__main__":
     main()
