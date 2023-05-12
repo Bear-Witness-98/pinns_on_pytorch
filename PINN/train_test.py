2edbb137ab584396fcc28768222a364246caf907
Santiago <santiago@tryolabs.com>
2023-05-12 15:40:38 +0000

    Update train_test loop and Validation in main.ipynb

--- PINN/train_test.py
+++ PINN/train_test.py
@@ -1,16 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 
+BATCH_SIZE = 10
+
 
 def train_loop(
-    dataloader: DataLoader, model: nn.Module, loss_fn: nn, optimizer: torch.optim
+    input_data: torch.Tensor, model: nn.Module, loss_fn: nn, optimizer: torch.optim
 ) -> None:
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
+    # I must do the "dataloader" myself, as all the data is to be stored in RAM
+    # i.e. in a torch.Tensor variable.
+    num_datapoints = input_data.shape[0]
+    permutation = np.random.permutation(num_datapoints)
+    input_data_permuted = input_data[permutation]
+    batches = torch.split(input_data_permuted, BATCH_SIZE)
+    for batch_num, X in enumerate(batches):
         # compute prediction and loss
         pred = model(X)
-        loss = loss_fn(pred, y)
+        plt.figure()
+        plt.scatter(X.detach().numpy(), pred.detach().numpy())
+        plt.show()
+
+        """
 
         # backpropagation
         optimizer.zero_grad()
@@ -21,8 +34,12 @@ def train_loop(
             loss, current = loss.item(), (batch + 1) * len(X)
             print(f"Loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
 
+        """
+    return
+
 
 def test_loop(dataloader: DataLoader, model: nn.Module, loss_fn: nn) -> None:
+    """
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
@@ -37,3 +54,5 @@ def test_loop(dataloader: DataLoader, model: nn.Module, loss_fn: nn) -> None:
     print(
         f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
     )
+    """
+    return
