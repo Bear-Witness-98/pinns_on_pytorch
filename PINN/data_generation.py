b4fcd49673586ccef31d6853d926c7ff4b836e99
Santiago <santiago@tryolabs.com>
2023-05-12 14:59:11 +0000

    Modify data generation, net file. Add main testing notebook

--- PINN/data_generation.py
+++ PINN/data_generation.py
@@ -7,9 +7,15 @@ X0 = 1
 X_dot0 = 1
 
 
-def initial_conditions() -> list[list[float, float], list[float, float]]:
-    return [[T0, X0], [T0, X_dot0]]
+def get_initial_condition() -> list[list[float, float], list[float, float]]:
+    return {
+        "time-positon": (T0, X0),
+        "time-speed": (T0, X_dot0),
+    }
 
 
-def time_values(max_time: float = 2.0, num_samples: int = 50) -> torch.Tensor:
-    return torch.tensor(np.random.rand(num_samples) * max_time)
+def get_time_sampling(max_time: float = 2.0, num_samples: int = 50) -> torch.Tensor:
+    # need an input of shape (B, 1), being B the Batch size, and 1 is the only input
+    # dimension of the Net
+    time_samples = torch.tensor(np.sort(np.random.rand(num_samples) * max_time))
+    return time_samples.reshape(time_samples.shape[0], 1).type(torch.float)
---
+++ PINN/train_test.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+
+def train_loop(
+    dataloader: DataLoader, model: nn.Module, loss_fn: nn, optimizer: torch.optim
+) -> None:
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        # compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"Loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+
+def test_loop(dataloader: DataLoader, model: nn.Module, loss_fn: nn) -> None:
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(
+        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+    )
